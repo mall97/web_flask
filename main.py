f568219ce92a729e67c6a83b6a7b1f7436a4a4f9
miguel lopes <miguellopes1997.ml@gmail.com>
2021-03-07 23:46:11 +0000

    new changes

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "test"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    email = db.Column(db.String(100))
+    
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/view")
+def view():
+    return render_template("view.html", values=users.query.all())
+
 
 @app.route("/login", methods= ['GET' , 'POST' ])
 def login():
@@ -15,25 +33,53 @@ def login():
         session.permanent = True
         user = request.form.get("nm", None)
         session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, None)
+            db.session.add(usr)
+            db.session.commit()
+
+        flash("Login succesful!", "info")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("Already logged in!", "info")
             return redirect(url_for("user"))
         
         return render_template("login.html")
 
-@app.route("/user")
+@app.route("/user", methods= ['GET' , 'POST' ])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email was save")
+        else:
+            if "email" in session:
+                email = session["email"]
+
+        return render_template("user.html", email=email)
     else:
+        flash("you are not logged in")
         return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
     session.pop("user", None)
+    session.pop("email", None)
+    flash("You have been log out!", "info")
     return redirect(url_for("login"))
     
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
